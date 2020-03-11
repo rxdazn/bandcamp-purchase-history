7638059461e1b33b184c48192aac7732505dd245
rxdazn <rxdazn@gmail.com>
2020-03-10 22:21:55 -0400

    Add download link instead of triggering automatic download
    Allows you to name the save file

--- popup.js
+++ popup.js
@@ -1,3 +1,14 @@
+// Injects a `script` tag into the purchase page body that creates a `div`
+// to store the crumb for the `/api/orderhistory/1/get_items endpoint`.
+// This is necessary as the `_crumbs` variable is defined on the purchase page
+// as a variable.
+// Chrome extension popups do not have access a page's context - only to
+// its DOM; injecting a script into the page's DOM and creating an element that
+// stores data (via data attributes/element.dataset) makes it so the extension's
+// popup's javascript can read that data added to the page's DOM.
+//
+// See Content Scripts:
+// https://developer.chrome.com/extensions/content_scripts
 function createCrumbsDataElement() {
   let createCrumbsDataElementScript = `let div = document.createElement('div'); div.id = 'rx-bc-crumbs-data'; div.dataset.getItemsCrumb = _crumbs['api/orderhistory/1/get_items']; document.documentElement.appendChild(div);`;
 
@@ -7,7 +18,6 @@ function createCrumbsDataElement() {
       (() => {
         let scriptElement = document.createElement('script');
         scriptElement.text = "${createCrumbsDataElementScript}";
-        console.log('scriptElement', scriptElement);
         document.documentElement.appendChild(scriptElement);
       })();
     `},
@@ -31,7 +41,7 @@ function processPageData(orders, exportedRowsNames) {
   let ordersRows = orders.map(order => {
     let exported = [];
     exportedRowsNames.forEach(exportedRowName => {
-      exported.push(order[exportedRowName] || '-');
+      exported.push(window.encodeURIComponent(`"${order[exportedRowName]}"`) || '-');
     });
     return exported;
   });
@@ -60,12 +70,13 @@ function updateProgress(processedOrders, totalOrders) {
 async function processOrders(pageData, getItemsCrumb) {
   let EXPORTED_ROWS = [
     'payment_date',
+    'bandcamp_id',
     'artist_name',
     'item_title',
+    'quantity',
     'unit_price',
     'tax',
     'tax_type',
-    'quantity',
     'currency',
     'card_brand',
     'card_num',
@@ -77,43 +88,45 @@ async function processOrders(pageData, getItemsCrumb) {
   let totalOrders = pageData['orderhistory']['total_items']
   let processedOrders = 0;
   let ordersRows = []
- 
+
   let formData = {
     'username': pageData['orderhistory']['username'],
     'platform': pageData['orderhistory']['platform'],
-    'last_token': pageData['orderhistory']['last_token'],
-    'crumb': getItemsCrumb,
+    'last_token': pageData['orderhistory']['last_token'], // pagination token for the most recently fetched order
+    'crumb': getItemsCrumb, // validation token, specific per API endpoint, expires after some time
   };
+  // TODO: refresh crumb via 400 server response
+  // when an API request is sent with an invalid crumb, the server responds with
+  // a new valid one.
 
   while (orders.length) {
-    processedOrders += orders.length;
     ordersRows.push(...processPageData(orders, EXPORTED_ROWS));
+    processedOrders += orders.length;
     updateProgress(processedOrders, totalOrders);
-    pageData = await fetchPageData(formData); 
-    console.log('pageData', pageData);
+
+    pageData = await fetchPageData(formData);
     formData['last_token'] = pageData['last_token'];
     orders = pageData['items'];
-    await sleep();
+    await sleep(); // add sleep to simulate user scrolling and not hammer the server
   }
 
   let csvContent = "data:text/csv;charset=utf-8,";
 
   ordersRows.splice(0, 0, EXPORTED_ROWS); // headers
   ordersRows.forEach(row => {
-    contentRow = row.join(";");
+    contentRow = row.join(window.encodeURIComponent(";")); // escaping ';' delimiter as it's reserved for URIs
+    console.log('add row to csv', contentRow);
     csvContent += `${contentRow}\n`;
   });
 
-  window.open(csvContent);
+  let downloadLink = document.querySelector("#download-link");
+  let date = new Date().toISOString().split('T')[0]; // iso string date e.g: 2020-03-11T02:09:22.833Z
+  downloadLink.style.display = 'block';
+  downloadLink.href = csvContent;
+  downloadLink.download = `${date}_bandcamp_purchases_${totalOrders}_orders.csv`;
 }
 
 (() => {
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log('received message', request, sender, sendResponse);
-    sendResponse({"ok": "bye"});
-    //document.querySelector('#progress-text').innerText = `${evt.processedOrders} / ${evt.totalOrders}`;
-  });
-
   document.addEventListener("DOMContentLoaded", function(evt) {
     let loadHistoryButton = document.querySelector('#load');
     let scriptElement = document.createElement('script');
@@ -134,15 +147,16 @@ async function processOrders(pageData, getItemsCrumb) {
         null,
         {code:
           `
-          let data = {
-            getItemsCrumb: document.querySelector('#rx-bc-crumbs-data').dataset.getItemsCrumb,
-            pageData: JSON.parse(document.querySelector("#pagedata").dataset["blob"])
-          }; console.log('data', data);
-          data
-          `
+            var bcData = {
+              getItemsCrumb: document.querySelector('#rx-bc-crumbs-data').dataset.getItemsCrumb,
+              pageData: JSON.parse(document.querySelector("#pagedata").dataset["blob"])
+            };
+            bcData
+          ` // `chrome.tabs.executeScript` in `code` mode doesn't accept `return` statements.
+            // To send data to the `results` callback, you need to reference the
+            // variable you want to return in the last statement of the code string.
         },
         (results) => {
-          console.log('results', results);
           processOrders(results[0].pageData, results[0].getItemsCrumb);
         });
     };
